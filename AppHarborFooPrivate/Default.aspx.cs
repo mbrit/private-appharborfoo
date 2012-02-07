42d84ee11040ecd8fc245433fcd9703d08a3bc2b
unknown <Matthew@AMXVMBR.(none)>
2012-02-07 22:26:29 +0000

    First commit

---
+++ AppHarborFooPrivate/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AppHarborFooPrivate
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public string Message
+        {
+            get
+            {
+                return "Welcome to this private AppHarbor repo - " + DateTime.UtcNow.ToString();
+            }
+        }
+    }
+}
